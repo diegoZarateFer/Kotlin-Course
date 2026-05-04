339c6cf198fa98a80234be9478c922f7479bb3ff
diegoZarate <diego.zarate@memphis.mx>
2026-05-04 11:32:55 -0600

    First commit.

---
+++ src/Rating.kt
@@ -0,0 +1,10 @@
+fun main() {
+
+    print("Enter your rating (1 - 5): ")
+    val rating = readln()
+
+    print("Leave a comment: ")
+    val comment = readln()
+
+    print("Thank you for your review!\n You gave us a ratig of: $rating \n Your Comment: $comment")
+}
---
+++ src/ReadingFromConsole.kt
@@ -0,0 +1,18 @@
+fun main() {
+    print("Input your name: ")
+    val name = readln()
+
+    print("Enter your age: ");
+    val age = readln()
+
+    print("Enter your gender: ");
+    val gender = readln()
+
+    print("Enter your height: ");
+    val height = readln()
+
+    print("Enter your weight: ");
+    val weight = readln()
+
+    print("Name: $name \nAge: $age \nGender: $gender \nHeight: $height \nWeight: $weight")
+}
