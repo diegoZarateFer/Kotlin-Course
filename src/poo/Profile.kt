ed48a8577bd0d2db47e594ca183ae39cdef65e9d
diegoZarate <diego.zarate@memphis.mx>
2026-05-04 14:43:00 -0600

    Classes and Objects.

---
+++ src/poo/Profile.kt
@@ -0,0 +1,24 @@
+package poo
+
+fun main() {
+    print("Name: ")
+    val name = readln()
+
+    print("Age: ")
+    val age = readln().toInt()
+
+    val personA = Person()
+    val personB = Person()
+
+    personA.name = name
+    personA.age = age
+
+    print("Person B name: ")
+    personB.name = readln()
+
+    print("Person B age: ")
+    personB.age = readln().toInt()
+
+    println("Name = ${personA.name} \nAge = ${personA.age}")
+    println("Name = ${personB.name} \nAge = ${personB.age}")
+}
